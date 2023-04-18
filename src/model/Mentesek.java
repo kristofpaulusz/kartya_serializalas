cf0c23db1a0cae87076856652944554a84cbbeef
kristofpaulusz <paulusz.kristof.csanad@diak.szamalk-szalezi.hu>
2023-04-18 13:08:59 +0200

    szerializalhato

--- src/model/Mentesek.java
+++ src/model/Mentesek.java
@@ -1,8 +1,10 @@
 package model;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -13,29 +15,13 @@ public class Mentesek {
 
     ArrayList<Kartya> kartyak = new ArrayList<>();
     public static void main(String[] args) {
-//        feltoltes();
         new Mentesek().feladat();
-        
-//        
-//        try {
-//            FileOutputStream fOS = new FileOutputStream("allapot.txt");
-//            ObjectOutputStream oOS = new ObjectOutputStream(fOS);
-//            for (Kartya kartya : kartyak) {
-//                oOS.writeObject(kartya);
-//            }
-//            oOS.close();
-//            
-//            
-//        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(Mentesek.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (IOException ex) {
-//            Logger.getLogger(Mentesek.class.getName()).log(Level.SEVERE, null, ex);
-//        }
     }
 
     private void feladat() {
        feltoltes();
        mentes();
+       beolvasas();
     }
     private void feltoltes() {
         Kartya k1 = new Kartya("Várnai Csata", "1444");
@@ -49,19 +35,35 @@ public class Mentesek {
 
     private void mentes() {
     try {
-            FileOutputStream fOS = new FileOutputStream("allapot.txt");
+            FileOutputStream fOS = new FileOutputStream("allapot.ser");
             ObjectOutputStream oOS = new ObjectOutputStream(fOS);
-            for (Kartya kartya : kartyak) {
-                oOS.writeObject(kartya);
-            }
+            oOS.writeObject(kartyak);
+//            for (Kartya kartya : kartyak) {
+//                oOS.writeObject(kartya);
+//            }
             oOS.close();
-            
+            fOS.close();
             
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Mentesek.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(Mentesek.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    private void beolvasas() {
+        try {
+            ObjectInputStream fIS = new ObjectInputStream(new FileInputStream("allapot.ser"));
+            ArrayList<Kartya> newKartyak = (ArrayList<Kartya>) fIS.readObject();
+            fIS.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Mentesek.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Mentesek.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Mentesek.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
     }
     
 }
